c47e93756876de08bc532c6225c04d9a7fc4c6b0
abdulmk184-ux <abdulmk184@gmail.com>
2026-06-26 09:52:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,26 @@
+import asyncio
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-TOKEN = "8905509516:AAGFTLo7hg4DFrN2BgzV82B36W2ZX1EAWc4"
+TOKEN = "YAHAN_APNA_NAYA_BOT_TOKEN_DALO"
 
 async def photo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Manga image receive ho gayi!")
 
-app = Application.builder().token(TOKEN).build()
+async def main():
+    app = Application.builder().token(TOKEN).build()
 
-app.add_handler(MessageHandler(filters.PHOTO, photo_handler))
+    app.add_handler(
+        MessageHandler(filters.PHOTO, photo_handler)
+    )
 
-print("Bot Running...")
-app.run_polling()
+    print("Bot Running...")
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    while True:
+        await asyncio.sleep(3600)
+
+if __name__ == "__main__":
+    asyncio.run(main())
