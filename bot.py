1850154108c209f013288158e2e0ed26c796b343
abdulmk184-ux <abdulmk184@gmail.com>
2026-09-22 18:36:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TOKEN = "8905509516":AAHCAPiDOPA3IseS6TI2WD
 
-ZE97a6m6TExjc"
+ZE97a6m6TExjc
 
 async def photo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Manga image receive ho gayi!")
